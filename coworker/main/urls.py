7dc06b9b651b4bed8b5ee65ac49e11900019ce7f
Alex Stupak <stupak.alexn@gmail.com>
2017-09-11 23:58:52 +0300

    added profile

--- coworker/main/urls.py
+++ coworker/main/urls.py
@@ -15,5 +15,8 @@ urlpatterns = [
     url(r'^privacy-policy/$', TemplateView.as_view(template_name='pages/privacy-policy.html'), name='privacy-policy'),
     url(r'^help/$', TemplateView.as_view(template_name='pages/help.html'), name='help'),
     url(r'^search/$', view=views.SearchList.as_view(), name='search'),
-    url(r'^list-space/$', view=views.ListSpace.as_view(), name='list-space')
+    url(r'^list-space/$', view=views.ListSpace.as_view(), name='list-space'),
+
+    #ineral
+    url(r'^profile/$', view=TemplateView.as_view(template_name='pages/profile.html'), name='profile')
 ]
