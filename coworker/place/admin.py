3949a80ee9422ab5e60ae208d239cfad67852e04
Artem Hruzd <arti3d.artem@gmail.com>
2017-09-26 19:11:22 +0100

    artem: fixed validation fo form, rewrite photo form

--- coworker/place/admin.py
+++ coworker/place/admin.py
@@ -7,16 +7,20 @@ class AmenitiesAdmin(admin.ModelAdmin):
     list_editable = ['is_additional']
 
 
-class MeetingRoomAdmin(admin.StackedInline):
+class MeetingRoomInlineAdmin(admin.StackedInline):
     model = MeetingRoom
 
 
-class MembershipDeskPriceAdmin(admin.StackedInline):
+class MembershipDeskPriceInlineAdmin(admin.StackedInline):
     model = MembershipDeskPrice
 
 
+class PhotosInlineAdmin(admin.StackedInline):
+    model = Photos
+
+
 class PlaceAdmin(admin.ModelAdmin):
-    inlines = [MeetingRoomAdmin, MembershipDeskPriceAdmin]
+    inlines = [MeetingRoomInlineAdmin, MembershipDeskPriceInlineAdmin, PhotosInlineAdmin]
     # list_display = ('timestamp', application__user, 'amount', 'receipt_image', 'logged')
     # list_filter = ('application__user', )
     # search_fields = ('application__user__first_name', 'application__user__last_name', 'application__user__email')
