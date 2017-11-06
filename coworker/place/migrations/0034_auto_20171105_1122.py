55ef0f4bbfbc4486bf94c53eda60441a463799f0
Alex Stupak <stupak.alexn@gmail.com>
2017-11-06 23:28:46 +0200

    update

--- coworker/place/migrations/0034_auto_20171105_1122.py
+++ coworker/place/migrations/0034_auto_20171105_1122.py
@@ -22,7 +22,7 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='place',
             name='category',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='place.Category'),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='place.Category'),
         ),
         migrations.AlterField(
             model_name='place',
